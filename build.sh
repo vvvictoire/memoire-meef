32bfbf25831c138d407574a9e6a207cae7e620ad
Victoire <vvictoire@pm.me>
2019-01-27 19:57:13 +0100

    Les bases du document sont posées ! On va pouvoir rédiger

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/bash
 FOLDER=main
-FILE=main
+FILE=henriette_jidouard_milis_2018_19_MEMOIRE\ MEEF
 
 # change into the desired folder
 cd $FOLDER
 
 # remove pdf if present
-[ -f $FILE.pdf ] && rm $FILE.pdf || echo "continue without remove"
+[ -f "$FILE.pdf" ] && rm "$FILE.pdf" || echo "continue without remove"
 
 # test if pdf is removed, fail if still present
-[ -f $FILE.pdf ] && exit 1 || echo "continue building pdf output"
+[ -f "$FILE.pdf" ] && exit 1 || echo "continue building pdf output"
 
 
 # build pdf from source
-xelatex $FILE.tex
-bibtex $FILE.aux
-xelatex $FILE.tex
-xelatex $FILE.tex
+xelatex "$FILE.tex"
+bibtex "$FILE.aux"
+xelatex "$FILE.tex"
+xelatex "$FILE.tex"
 
 # exit successfully if pdf is present or with error if not present
-[ -f $FILE.pdf ] && exit 0 || exit 1
+[ -f "$FILE.pdf" ] && exit 0 || exit 1
---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -f main/*.{aux,bbl,blg,log,pdf}
+rm -f main/sections/*.aux
